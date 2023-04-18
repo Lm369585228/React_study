aae9924d1f6f0abe8dfdc4983cea3adab32a15c0
liaomin123 <369585228@qq.com>
2023-04-18 22:54:24 +0800

    router学习

---
+++ 18._React脚手架/react-demo/19_src_withRouter组件的使用/pages/Home/Message/Details/index.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+// import qs from 'qs'
+
+export default class Detils extends Component {
+    state = {
+        contents: [
+            { id: '01', content: "小廖" },
+            { id: '02', content: "小金" },
+            { id: '03', content: "西奥多" },
+        ]
+    }
+    render() {
+        // console.log(this.props);
+        // 接收params参数
+        // const {id,text} = this.props.match.params
+        //接收search参数
+        // const {search} = this.props.location
+        // const {id,text} = qs.parse(search.slice(1))
+        //接收state参数
+        const {id,text} = this.props.location.state || {}
+        const contentResult = this.state.contents.find((contentsObj) => {
+            return contentsObj.id === id
+        }) || {}
+        return (
+            <ul>
+                <li>id:{id}</li>
+                <li>text:{text}</li>
+                <li>content:{contentResult.content}</li>
+            </ul>
+        )
+    }
+}
